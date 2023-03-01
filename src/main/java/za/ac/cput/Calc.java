8195a4aee68038b48032ec42837430c74aad8331
Jordy Meye <220072841@mycput.ac.za>
2023-03-01 10:49:43 +0200

    Added method to find the sum of two real numbers

--- src/main/java/za/ac/cput/Calc.java
+++ src/main/java/za/ac/cput/Calc.java
@@ -6,9 +6,21 @@ public class Calc {
      * @param a
      * @return
      */
-    public static int add(int a int b)
+    public static int add(int a, int b)
     {
         return (a +b);
 
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+
+    public static double add(double a , double b){
+
+        return (a+b);
     }
 }
